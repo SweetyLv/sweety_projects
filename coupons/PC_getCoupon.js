a488cffaab504bcd868be146f437426c1007e06c
lovely_sweety <445294960@qq.com>
2017-11-18 23:11:05 +0800

    Update PC_getCoupon.js

--- coupons/PC_getCoupon.js
+++ coupons/PC_getCoupon.js
@@ -3,7 +3,6 @@ var dd = $(".coupon_tiket li").length;
 function appendText(){
 	var aText3 = document.createElement("a");
 	//aText3.innerHTML="<img src="${PCRESOURCE_PATH}res/PCimg/qidai.png">";
-	aText3.innerHTML='<img src="http://res.gbhui.com/res/PCimg/qidai.png">';
 	$(".wait_content").append(aText3);
 	$(".wait_content a").addClass("sha_1711 wait_coupon");
 }
